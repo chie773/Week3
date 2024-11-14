5113a8d2c9c569a99537d34b142105fadec82d9a
chie773 <168461633+chie773@users.noreply.github.com>
2024-11-14 12:09:50 -0500

    Update Power.java

--- src/Power.java
+++ src/Power.java
@@ -1,9 +1,9 @@
 class Power {
     public double pow(double x, int n) {
-        if (n == 1) {
+        if (n == 1) { // This base case checks to see if n reaches 1, so it can return x because x to the power of 1 is x
             return x;
         }
 
-        return x * pow(x, n-1);
-    }
+        return x * pow(x, n-1); // This recursive step multiplies x by the function when x is to the n-1 power. This 
+    }                            // makes it simpler in terms of dividing the problem into concurrent sub problems
 }
