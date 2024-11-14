1f93c001e8313010b4ab5dac32a2d923f8d84ff5
chie773 <168461633+chie773@users.noreply.github.com>
2024-11-14 12:08:06 -0500

    Update PowerTen.java

--- src/PowerTen.java
+++ src/PowerTen.java
@@ -1,13 +1,12 @@
 class PowerTen {
     public boolean isPowerOfTen(int n) {
-        while (n > 1) { // This is the base condition, it terminates if n reaches 1 or 0, it will be a power of 10
-            if (n % 10 != 0) { // This checks if the last number in n is any number from 2-9 and returns false
-                return false;
-            } else {
-                n /= 10; // This divides n by 10 to decrease it to its ones place
-            }
+        if (n == 1 || n == 0) { // This base case checks to see if n reached 1 or 0, this signifies its a power of 10.
+            return true;
         }
+        if (n % 10 != 0) { // This checks for if the last number in n doesn't have a remainder of 0, 
+            return false; // which signifies it not being a power of 1
+        } 
 
-        return true; // Returns true because if the loop terminates then the number is a power of ten
+        return isPowerOfTen(n/10); // This recursive steps decreases n / 10, to check each integer in n
     }
 }
